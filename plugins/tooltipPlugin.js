99e409c8ff12c799623e6f235474a7dd2766fa7b
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-11-10 18:08:55 +0100

    feat(main) tooltip class in tailwind

---
+++ plugins/tooltipPlugin.js
@@ -0,0 +1,29 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(({ addUtilities, theme, variants }) => {
+  const tooltipStyles = {
+    '.tooltip': {
+      position: 'relative',
+      display: 'inline-block',
+    },
+    '.tooltip .tooltiptext': {
+      visibility: 'hidden',
+      width: '120px',
+      backgroundColor: 'black',
+      color: '#fff',
+      textAlign: 'center',
+      padding: '5px 0',
+      borderRadius: '6px',
+      position: 'absolute',
+      zIndex: '1',
+      top: '-500%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+    },
+    '.tooltip:hover .tooltiptext': {
+      visibility: 'visible',
+    },
+  };
+
+  addUtilities(tooltipStyles, variants('tooltip'));
+});
