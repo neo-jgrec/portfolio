929c0b709e909541151451b39e1181750ea57865
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-11-11 00:06:17 +0100

    feat(ProjectPage) add contributors and gh infos

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -9,10 +9,14 @@ import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import Theme from 'react-syntax-highlighter/dist/esm/styles/prism/one-dark';
 
+import logoGh from '../svg/github-mark-white.svg';
+
 function ProjectPage() {
   const { projectName } = useParams();
   const [markdown, setMarkdown] = React.useState('');
   const [project, setProject] = React.useState(null);
+  const [apiData, setApiData] = React.useState(null);
+  const [contributors, setContributors] = React.useState(null);
 
   React.useEffect(() => {
     if (project)
@@ -32,7 +36,23 @@ function ProjectPage() {
     }
   }, [project]);
 
-  if (!project)
+  React.useEffect(() => {
+    if (project) {
+      fetch(project.github)
+        .then((response) => response.json())
+        .then((data) => setApiData(data));
+    }
+  }, [project]);
+
+  React.useEffect(() => {
+    if (project) {
+      fetch(project.github + '/contributors')
+        .then((response) => response.json())
+        .then((data) => setContributors(data));
+    }
+  }, [project]);
+
+  if (!project || !apiData || !contributors)
     return null;
 
   return (
@@ -96,6 +116,42 @@ function ProjectPage() {
           }
         }}
       />
+
+      <br /> <br />
+
+      <div className="flex flex-row justify-between items-center px-4 lg:px-52">
+        {contributors && contributors.map((contributor, index) => (
+          <a
+            key={index}
+            className="flex flex-col items-center p-4"
+            href={contributor.html_url}
+            target="_blank" rel="noreferrer"
+          >
+            <img src={contributor.avatar_url} alt={contributor.login} className="w-16 h-16 mr-2 rounded-full object-cover" style={{ imageRendering: 'pixelated' }} />
+            <p className="text-lg font-bold text-white items-center">
+              {contributor.login}
+            </p>
+          </a>
+        ))}
+      </div>
+
+      <br /> <br />
+
+      <div className="flex flex-row justify-between px-4">
+        <a
+          className="flex flex-row items-center p-4 rounded-lg shadow-lg hover:bg-stone-50 hover:bg-opacity-5 transition duration-300 ease-in-out"
+          href={apiData?.html_url}
+          target="_blank" rel="noreferrer"
+        >
+          <img src={logoGh} alt="GitHub Logo" className="w-6 h-6 mr-2" />
+          <p className="text-xl font-bold text-white items-center">
+            {project.name} repository
+          </p>
+        </a>
+        <p className="font-bold text-xl text-white items-center flex">
+          Creation of the repository : {new Date(apiData?.created_at).toLocaleDateString('en-BR', { year: 'numeric', month: 'long', day: 'numeric' })}
+        </p>
+      </div>
     </Layout>
   );
 }
