8cbc33da0d2ad4da44d2ef1867138fcaba0a29a5
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-11-11 18:49:44 +0100

    refactor(ProjectCard) put the component into his file

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Projects from "./Projects";
+import ProjectCard from "./ProjectCard";
 
 export {
     Header,
     Footer,
     Projects,
+    ProjectCard
 }
